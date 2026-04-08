ee7475b4a845c0f54dbe8b187a7e0a28cffb722e
gustopp <gustopp@users.noreply.github.com>
2026-04-08 09:11:25 -0300

    Usar logo de encabezado, optimizar vista móvil y quitar código eDreams

--- script.js
+++ script.js
@@ -78,7 +78,6 @@ document.addEventListener("DOMContentLoaded", () => {
         let metaHtml = `
             <div class="flight-meta">
                 ${vuelo.codigo_reserva_aerolinea ? `<span class="badge badge-highlight">Código de reserva: ${vuelo.codigo_reserva_aerolinea}</span>` : ''}
-                ${vuelo.codigo_reserva_edreams ? `<span class="badge">eDreams: ${vuelo.codigo_reserva_edreams}</span>` : ''}
             </div>
         `;
 
